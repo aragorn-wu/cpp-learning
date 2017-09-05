7f303b16e2a8e90a401ef9619056e05613a39686
deadking01 <wuqingfeng234@163.com>
2017-09-05 17:11:52 +0800

    basic address

---
+++ di-4-zhang-fu-he-lei-xing/address.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    using namespace std;
+    int donuts=6;
+    double cups =4.5;
+    cout <<"double value = "<<donuts<<" and donts address= "<<&donuts<<endl;
+    cout <<"cups value= "<<cups<<" and cups address = "<<&cups <<endl;
+    return 0;
+}
